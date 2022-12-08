5a4ba4aae619f6e19253633fb5daec0a7cb26343
poojama123 <120040790+poojama123@users.noreply.github.com>
2022-12-08 10:41:05 -0500

    Update TimeEntry.java

--- core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
+++ core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
@@ -3,7 +3,7 @@ package training.taylor.timetracker.core.dao;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-/**
+/*
  * Created by Jason on 6/19/2015.
  */
 @Component
